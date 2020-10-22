1df8694a15bc0b37d18568bb2729d9a89cd9ba3e
Harikrishnan R <anoob.bava@rubyians.com>
2020-10-22 17:35:37 +0530

    first_commit

--- my_mongo_app/app/controllers/api/user_controller.rb
+++ my_mongo_app/app/controllers/api/user_controller.rb
@@ -13,7 +13,7 @@ class Api::UserController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: created
+      render json: @user, status: :created
     else
       render json: {errors: @user.errors.full_messages, status: :unprocessable_entity }
     end
--- my_mongo_app/app/controllers/application_controller.rb
+++ my_mongo_app/app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  skip_befor_action :verify_authenticity_token
 end
