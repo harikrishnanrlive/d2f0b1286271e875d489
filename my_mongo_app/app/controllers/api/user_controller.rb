037fa01587fac044c5060dcba6d3d28dd90ff458
Harikrishnan R <anoob.bava@rubyians.com>
2020-10-22 17:38:53 +0530

    test

--- my_mongo_app/app/controllers/api/user_controller.rb
+++ my_mongo_app/app/controllers/api/user_controller.rb
@@ -11,7 +11,7 @@ class Api::UserController < ApplicationController
   end
 
   def create
-    @user =  User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
       render json: @user, status: :created
     else
