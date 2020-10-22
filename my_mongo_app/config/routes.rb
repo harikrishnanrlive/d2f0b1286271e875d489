0e59ab1305bf845276c679f6cb8145be81ad4f8e
Harikrishnan R <anoob.bava@rubyians.com>
2020-10-22 17:08:52 +0530

    first_commit

--- my_mongo_app/config/routes.rb
+++ my_mongo_app/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :user, only[:show, :create, :update, :destroy]
+    get 'users' => 'users#index'
+    get 'typehead/:input' => 'users#index'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
