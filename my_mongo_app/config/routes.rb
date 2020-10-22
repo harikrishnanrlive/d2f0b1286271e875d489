996330fd57385705efeef75464f1008d357186da
Harikrishnan R <anoob.bava@rubyians.com>
2020-10-22 17:12:31 +0530

    first_commit

--- my_mongo_app/config/routes.rb
+++ my_mongo_app/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :user, only[:show, :create, :update, :destroy]
+    resources :user, only: [:show, :create, :update, :destroy]
     get 'users' => 'users#index'
     get 'typehead/:input' => 'users#index'
   end
